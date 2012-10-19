a26a5c396fd40f38ed207fc5e26d114f929c3114
Doug Patti <douglas.patti@gmail.com>
2012-10-19 16:26:18 -0400

    Initial customer model

--- app/customer/models.py
+++ app/customer/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.model):
+  full_name    = models.CharField(max_length = 50)
+  email        = models.EmailField(max_length = 75)
+  student_id   = models.IntegerField()
+  phone_number = models.CharField(max_length = 12)
