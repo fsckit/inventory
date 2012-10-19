b4e93348463124a3241381c24d72f8965cbca661
Doug Patti <douglas.patti@gmail.com>
2012-10-19 17:00:22 -0400

    Add base urls

--- app/customer/views.py
+++ app/customer/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from app.decorators.json_response import *
+
+@json_response
+def create(request):
+  return {}
+
+@json_response
+def read(request, id = -1):
+  return {}
+
+@json_response
+def update(request, id = -1):
+  return {}
+
+@json_response
+def delete(request, id = -1):
+  return {}
--- app/urls/base.py
+++ app/urls/base.py
@@ -1,17 +1,18 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'inventory.views.home', name='home'),
-    # url(r'^inventory/', include('inventory.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^$', 'app.views.index', name='home'),
+    (r'^staff/',       include('app.staff.urls')),
+    (r'^customer/',    include('app.customer.urls')),
+    (r'^item/',        include('app.item.urls')),
+    (r'^transaction/', include('app.transaction.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    # (r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    # This should be handled in production by nginx
+    url(r'^static/(?P<path>.*)$','django.views.static.serve', {'document_root': settings.STATIC_ROOT,'show_indexes': True}),
+    url(r'^(?P<path>favicon.ico)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,'show_indexes': False}),
 )
