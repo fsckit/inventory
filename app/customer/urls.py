00b83805d2fe0b1bc6dde3462f0108235995a6e5
Doug Patti <douglas.patti@gmail.com>
2012-10-19 17:19:41 -0400

    Expand on example customer views

--- app/customer/urls.py
+++ app/customer/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('app.customer.views',
   url(r'^create$', 'create', name='customer_create'),
-  url(r'^(\d+)$', 'read', name='customer_show'),
+  url(r'^(\d+)$', 'read', name='customer_read'),
   url(r'^(\d+)/update$', 'update', name='customer_update'),
   url(r'^(\d+)/delete$', 'delete', name='customer_delete'),
 )
--- app/customer/views.py
+++ app/customer/views.py
@@ -2,16 +2,16 @@ from app.decorators.json_response import *
 
 @json_response
 def create(request):
-  return {}
+  return { 'route': request.path }
 
 @json_response
 def read(request, id = -1):
-  return {}
+  return { 'route': request.path }
 
 @json_response
 def update(request, id = -1):
-  return {}
+  return { 'route': request.path }
 
 @json_response
 def delete(request, id = -1):
-  return {}
+  return { 'route': request.path }
