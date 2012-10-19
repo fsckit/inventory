4f94a2f10534f2fb1a83b1340558627759ab4033
Brandon Waite <brandon.waite92@gmail.com>
2012-10-19 17:25:10 -0400

    initial transaction models and controllers

--- app/transaction/models.py
+++ app/transaction/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.model):
+  ACTIONS = (
+    (u'b',	u'borrow'),
+	(u'l',	u'lend'),
+	(u'r',	u'return'),
+	(u'c',	u'claim'),
+  )
+  date      = models.DateTime(auto_now=True)
+  action    = models.CharField(max_leng=2, choices=ACTIONS)
