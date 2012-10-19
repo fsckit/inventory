cc1b4bd75a0b2852e5571b51a8b1a6c65f0418ec
Doug Patti <douglas.patti@gmail.com>
2012-10-19 17:06:59 -0400

    Add routes and controllers for customer

--- app/urls/base.py
+++ app/urls/base.py
@@ -1,18 +1,18 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 urlpatterns = patterns('',
-    url(r'^$', 'app.views.index', name='home'),
-    (r'^staff/',       include('app.staff.urls')),
-    (r'^customer/',    include('app.customer.urls')),
-    (r'^item/',        include('app.item.urls')),
-    (r'^transaction/', include('app.transaction.urls')),
+  url(r'^$', 'app.views.index', name='home'),
+  (r'^staff/',       include('app.staff.urls')),
+  (r'^customer/',    include('app.customer.urls')),
+  (r'^item/',        include('app.item.urls')),
+  (r'^transaction/', include('app.transaction.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+  # Uncomment the next line to enable the admin:
+  # (r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += patterns('',
-    # This should be handled in production by nginx
-    url(r'^static/(?P<path>.*)$','django.views.static.serve', {'document_root': settings.STATIC_ROOT,'show_indexes': True}),
-    url(r'^(?P<path>favicon.ico)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,'show_indexes': False}),
+  url(r'^static/(?P<path>.*)$','django.views.static.serve', {'document_root': settings.STATIC_ROOT,'show_indexes': True}),
+  url(r'^(?P<path>favicon.ico)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,'show_indexes': False}),
 )
