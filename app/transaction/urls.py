d7fa60e4ad23a9ddef5b90be2a4b02ffad2eb46f
Doug Patti <douglas.patti@gmail.com>
2012-10-21 17:09:25 -0400

    Add trailing slash to transaction urls

--- app/transaction/urls.py
+++ app/transaction/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('app.transaction.views',
-  url(r'^create$', 'create', name='transaction_create'),
-  url(r'^(\d+)$', 'read', name='ctransaction_read'),
-  url(r'^(\d+)/update$', 'update', name='transaction_update'),
-  url(r'^(\d+)/delete$', 'delete', name='transaction_delete'),
+  url(r'^create/$', 'create', name='transaction_create'),
+  url(r'^(\d+)/$', 'read', name='ctransaction_read'),
+  url(r'^(\d+)/update/$', 'update', name='transaction_update'),
+  url(r'^(\d+)/delete/$', 'delete', name='transaction_delete'),
 )
