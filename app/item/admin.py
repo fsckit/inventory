aae81ace260b782763362715587cad3893d42841
Doug Patti <douglas.patti@gmail.com>
2012-10-31 19:26:38 -0400

    Add admin interface for customers and items

---
+++ app/item/admin.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib import admin
+from app.item.models import Item
+
+class AdminForm(forms.ModelForm):
+    class Meta:
+        model = Item
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name','label_id','owning_customer','type')
+    form = AdminForm
+
+admin.site.register(Item, ItemAdmin)
