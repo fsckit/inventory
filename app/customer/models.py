c5452551245c1a6ea6207779b2657d906f7ef462
U-stangk-PC\stangk <stangk@stangk-PC.(none)>
2012-10-31 18:23:21 -0400

    Fixed model.model -> model.Model

--- app/customer/models.py
+++ app/customer/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Customer(models.model):
+class Customer(models.Model):
   full_name    = models.CharField(max_length = 50)
   email        = models.EmailField(max_length = 75)
   student_id   = models.IntegerField()
--- app/item/models.py
+++ app/item/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from app.customer.models import Customer
 
-class Item(models.model):
+class Item(models.Model):
   name         = models.CharField(max_length = 100)
   label_id     = models.IntegerField()
   owning_customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
