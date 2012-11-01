1dcb571e3da58853b1ccf5e9240c91d927036051
Kevin Stangl <stangk@rpi.edu>
2012-10-31 20:48:08 -0400

    Added transaction read

--- app/transaction/views.py
+++ app/transaction/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from app.transaction.models import Transaction
 
 def create(request):
   if request.method == 'POST':
@@ -17,9 +18,9 @@ def create(request):
     form = TransactionForm()
     return render_to_response('transaction/transaction_create.html', {'form': form}, context_instance=RequestContext(request))
 
-@json_response
 def read(request, id = -1):
-  return { 'route': request.path }
+  qs = Transaction.objects.filter(pk=id)
+  return render_to_response('transaction/transaction_read.html', {'o': qs[0]}, context_instance=RequestContext(request))
 
 @json_response
 def update(request, id = -1):
