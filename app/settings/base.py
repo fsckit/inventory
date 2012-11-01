ccb67772a48558173d46d8660d6a401e2694c438
Doug Patti <douglas.patti@gmail.com>
2012-11-01 16:57:08 -0400

    Move urls out of folder

--- app/settings/base.py
+++ app/settings/base.py
@@ -21,7 +21,7 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-ROOT_URLCONF = 'app.urls.base'
+ROOT_URLCONF = 'app.urls'
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
