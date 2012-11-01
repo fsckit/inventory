e49750693ebc53935f1b2062aaca2faea80bd19b
Brandon Waite <brandon.waite92@gmail.com>
2012-10-31 20:16:27 -0400

    Made Student ID's unique=True

--- app/customer/views.py
+++ app/customer/views.py
@@ -17,33 +17,20 @@ def create(request):
     form = CreateForm()
   return render_to_response('customer/customer_create.html', {'form': form}, context_instance=RequestContext(request))
 
-def read(request, id = -1):
+def read(request, id=-1):
   customer = Customer.objects.get(student_id=id)
   return render_to_response('customer/customer_read.html', {'customer': customer}, context_instance=RequestContext(request))
 
-def update(request, id = -1):
+def update(request, id=-1):
   if request.method == 'POST':
-    # If user not logged in, redirect to login
-    if not request.user.is_authenticated():
-      return HttpResponseRedirect('/')
-
     form = CreateForm(request.POST, request.FILES)
     if form.is_valid():
-      # TODO: This isn't exactly threadsafe
-      qs = Item.objects.filter(owning_customer__isnull, id=form.item_id)
-
-      # if qs is empty, then either the item does not exist or it's owned. 
-      if len(qs) != 0:
-        item = qs.pop()
-        item.owning_customer = id
-        item.save()
-        # display success
-      else:
-	qs = qs # empty statement so there's no error
-        # emit error to user
+      obj = form.save()
+      return HttpResponseRedirect(reverse('customer_read', args=[obj.student_id]))
   else:
-    form = TakeItemForm()
-  return render_to_response('customer/update.html', {'form': form}, context_instance=RequestContext(request))
+    customer = Customer.objects.get(student_id=id)
+    form = CreateForm(instance=customer)
+  return render_to_response('customer/customer_update.html', {'form': form}, context_instance=RequestContext(request))
 
 
 @json_response
