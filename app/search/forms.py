d0beb6473cde7115f503beb495c4cb2d309d61af
Kevin Stangl <stangk@rpi.edu>
2012-11-14 19:42:48 -0500

    SearchForm now only uses one paramter, qstr.

--- app/search/forms.py
+++ app/search/forms.py
@@ -1,9 +1,4 @@
 from django import forms
 
 class SearchForm(forms.Form):
-  # cust_info -> (items owned by, items held by)
-  cust_name = forms.CharField(required=False)
-
-  # item_info -> customers
-  item_name = forms.CharField(required=False)
-  item_label = forms.CharField(required=False)
+  qstr = forms.CharField()
--- app/search/views.py
+++ app/search/views.py
@@ -1,4 +1,5 @@
 import re
+import urllib
 
 from app.search.forms import SearchForm
 from app.item.models import Item
@@ -10,38 +11,25 @@ from django.template import RequestContext
 def query_terms(qstr):
   return [filter(None, x)[0] for x in re.compile('"([^"]+)"|(\S+)').findall(qstr)]
 
-
-
 def search(request):
-  if request.method == 'POST':
-    form = SearchForm(request.POST, request.FILES)
-    if form.is_valid():
-      item_name_q = query_terms(form.cleaned_data['item_name'])
-
-      iq = None
-      for term in item_name_q:
-        cq = Q(name__icontains=term)
-        if iq is None:
-          iq = cq
-        else:
-          iq = iq & cq
-      lbl = form.cleaned_data['item_label']
-      if lbl != '':
-        cq = Q(label_id=lbl)
-        if iq is None:
-          iq = cq
-        else:
-          iq = iq & cq
-
-      items = None
-      if iq is not None:
-        items = [(item.name, item.label_id, item.pk) for item in Item.objects.filter(iq)]
-
-      cust_name_q = form.cleaned_data['cust_name']
-      customers = None
-      if cust_name_q != '':
-        customers = [(customer.full_name, customer.email, customer.student_id) for customer in Customer.objects.filter(full_name__icontains=cust_name_q)]
-      return render_to_response('search/results.html', {'items': items, 'customers': customers}, context_instance=RequestContext(request))
+  form = SearchForm(request.GET, request.FILES)
+  if form.is_valid():
+    qterms = query_terms(form.cleaned_data['qstr'])
+    cq = None
+    iq = None
+    for term in qterms:
+      # Customer fullname LIKE [any qterm]
+      cq_inc = Q(full_name__icontains=term) | Q(email__icontains=term)
+      # Item {name, owner} LIKE [any qterm] OR label = [any qterm]
+      iq_inc = Q(name__icontains=term) | Q(label_id=term)
+      if cq is None: # implies iq = None
+        cq = cq_inc
+        iq = iq_inc
+      else: 
+        cq = cq | cq_inc
+        iq = iq | iq_inc
+    customers = [(customer.full_name, customer.email, customer.student_id) for customer in Customer.objects.filter(cq)]
+    items = [(item.name, item.label_id, item.pk) for item in Item.objects.filter(iq)]
+    return render_to_response('search/results.html', {'items': items, 'customers': customers}, context_instance=RequestContext(request))
   else:
-    form = SearchForm()
-  return render_to_response('search/search.html', {'form': form}, context_instance=RequestContext(request))
+    return render_to_response('search/search.html', {'form': form}, context_instance=RequestContext(request))
