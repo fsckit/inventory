b8a1dde05578015b36a4db72bdf10818d4edb7c1
Doug Patti <douglas.patti@gmail.com>
2012-11-17 04:37:31 -0500

    Extended javascript to submit inline

--- app/static/js/inventory.js
+++ app/static/js/inventory.js
@@ -1,11 +1,37 @@
-$(function(){
-  // View tabs
-  ['customer', 'item'].forEach(function(type){
-    $('#stage').on('show', '#' + type + ' .view', function(e) {
-      $(e.target.hash).load('/' + type + '/index');
+(function($){
+  // Events
+  $(function(){
+    // View tabs
+    ['customer', 'item'].forEach(function(type){
+      $('#stage').on('show', '#' + type + ' .view', function(e) {
+        $(e.target.hash).load('/' + type + '/index');
+      });
+    });
+
+    // Load history
+    $('#transaction-history').load('/transaction/index');
+
+    // Set up search/autocomplete fields
+    $(':input.search').search();
+
+    // Set up create buttons
+    $('.tab-pane.create form').on('submit', function(e){
+      var $this = $(this);
+      e.preventDefault();
+
+      $.ajax({
+        type: 'POST',
+        url: $this.attr('action'),
+        data: new FormData($this.get(0)),
+        dataType: 'json',
+        contentType: false,
+        processData: false,
+      })
+      .done(function(){
+        $this.get(0).reset();
+        $('#transaction :input:first').focus();
+      })
+      .fail($.err);
     });
   });
-
-  // Load history
-  $('#transaction-history').load('/transaction/index');
-});
+})(jQuery);
