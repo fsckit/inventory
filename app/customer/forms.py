4ae6c57fd70a91709489cee4bb33b57eef6a3f05
Chelsea Poirier <poiric@rpi.edu>
2012-11-17 16:48:17 -0500

    Index changes

--- app/customer/forms.py
+++ app/customer/forms.py
@@ -4,4 +4,4 @@ from app.customer.models import Customer
 class CreateForm(forms.ModelForm):
   class Meta:
     model = Customer
-    fields = ('full_name', 'email', 'student_id', 'phone_number')
+    fields = ('first_name', 'last_name', 'email', 'student_id', 'phone_number')
--- app/customer/models.py
+++ app/customer/models.py
@@ -2,9 +2,15 @@ from django.db import models
 
 class Customer(models.Model):
   student_id   = models.IntegerField(primary_key=True)
-  full_name    = models.CharField(max_length = 50)
+  first_name   = models.CharField(max_length = 50)
+  last_name    = models.CharField(max_length = 50)
   email        = models.EmailField(max_length = 75)
   phone_number = models.CharField(max_length = 12)
   
+  
+  def _get_full_name(self):
+    return '%s %s' % (self.first_name, self.last_name)
+  full_name = property(_get_full_name)
+
   def __str__(self):
     return str(self.full_name)
