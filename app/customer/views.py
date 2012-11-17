605b1e7dd9b3edcab238dcb49779c5b8d7a15b0d
Doug Patti <douglas.patti@gmail.com>
2012-11-17 01:17:55 -0500

    Class cleanup

--- app/customer/views.py
+++ app/customer/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from app.customer.forms import TakeItemForm
 from app.customer.forms import CreateForm
 from app.customer.models import Customer
 
