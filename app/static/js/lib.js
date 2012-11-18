efb7f48ec36e3c929d8d9db903c08b2223b68656
Doug Patti <douglas.patti@gmail.com>
2012-11-18 15:28:35 -0500

    Add message queueing system

--- app/static/js/lib.js
+++ app/static/js/lib.js
@@ -3,13 +3,55 @@
   window.Keys = { ENTER: 13, TAB: 9, UP: 38, DOWN: 40, ESC: 27 };
 
   // General page-wide error handler
-  $.error = function(err_string) {
-    console.error(err_string);
+  $.error = function(error) {
+    $.message(error, 'error');
   };
 
   // General page-wide message handler
   $.success = function(message) {
-    console.log(message);
+    $.message(message, 'success');
   };
+
+  $.message = (function(){
+    // Fade and lifetime in ms
+    var message_fade = 1 * 1000;
+    var message_life = 10 * 1000;
+    // Queue for messages to be displayed
+    var message_queue = [];
+    var $navbar = $('.navbar-inner');
+
+    var process_queue = function(){
+      if (message_queue.length == 0)
+        return;
+
+      // Display next message
+      var next = message_queue.shift();
+      $('<div>', { class: 'pull-left alert alert-' + next.type })
+        // Initially hide
+        .hide()
+        .text(next.message)
+        .appendTo($navbar)
+        // Fade in, wait, fade out
+        .fadeIn(message_fade)
+        .delay(message_life)
+        .fadeOut(message_fade, function(){ $(this).trigger('hide'); })
+        // When it is hidden, remove it and process next message if there is one
+        .on('hide', function(){
+          $(this).remove();
+          process_queue();
+        })
+        // When clicked, just hide immediately
+        .on('click', function(){
+          $(this).stop(true, true).hide().trigger('hide');
+        });
+    };
+
+    return function(message, type){
+      message_queue.push({message: message, type: type || 'info'});
+      if (message_queue.length == 1 && $navbar.find('.alert').length == 0) {
+        process_queue();
+      }
+    };
+  })();
 
 })(jQuery);
